ef93098e0019331ef6af8d3bfd35bdf22aedfaf7
arnoldliu8 <40721172+arnoldliu8@users.noreply.github.com>
2018-08-16 18:28:07 +0530

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -13,118 +13,323 @@ constructor(props){
     this.setState({mode:'students'})
   }
 
-
-
-
-
-
     render(){
         if(this.state.mode==="chem")
         return (
             <div>
-            <br/>
-            <br/>
-            <br/>
-            <div style={{height:"380px", width:"240px", border:"solid", borderColor:"#45CFE2", marginLeft:"50px"}}>
-            <div style={{height:"46px",width:"240px", backgroundColor:"#45CFE2",
-            margin: "auto",
-            textAlign:"center", color:"white"}}>  
-            <button style=
+            <div style=
             {{
-            borderRadius:"50%",
-            borderColor:"#D2B4DE", 
-            float:"left", 
-            height:"24px",
-            width:"28px", 
-            fontSize:"10px", 
-            backgroundColor:"#D2B4DE", 
-            position:"absolute",
-            left:"80px",
-            marginTop:"8px",
-            color:"white",
-            borderStyle:"solid"}}>HA</button>
+                height:"424px", 
+                width:"304px", 
+                backgroundColor:"#45CFE2",
+                position:"fixed",
+                left:"87px",
+                top:"168px",
+                borderStyle:"solid",
+                borderColor:"#45CFE2",
+                borderSize:"2px",
+            }}>
+
+            <div style=
+            {{
+                height:"42px",
+                width:"300px",
+                backgroundColor:"#45CFE2",
+                position:"fixed",
+                left:"89px",
+                top:"170px"
+            }}> 
+            
+            <div style=
+            {{
+            borderRadius:"50%",  
+            height:"23px",
+            width:"23px",  
+            backgroundColor:"#298089", 
+            position:"fixed",
+            left:"139px",
+            top:"177px"
+            }}><div style=
+            {{ 
+            height:"12px",
+            width:"14px",  
+            position:"fixed",
+            left:"142.17px",
+            top:"183.47px",
+            color:"#FFFFFF",
+            fontSize:"10.16px",
+            fontStyle:"Proxima Nova, Semibold",
+            textAlign:"left",
+            lineHeight:"12px"
+            }}>NK</div></div>
+
+            <div style=
+            {{
+            borderRadius:"50%",  
+            height:"23px",
+            width:"23px",  
+            backgroundColor:"#A967AA", 
+            position:"fixed",
+            left:"128px",
+            top:"177px"
+            }}><div style=
+            {{ 
+            height:"12px",
+            width:"14px",  
+            position:"fixed",
+            left:"132.17px",
+            top:"183.47px",
+            color:"#FFFFFF",
+            fontSize:"10.16px",
+            fontStyle:"Proxima Nova, Semibold",
+            textAlign:"left",
+            lineHeight:"12px"
+            }}>NK</div></div>
+
+
+            <div style=
+            {{
+            borderRadius:"50%",  
+            height:"23px",
+            width:"23px",  
+            backgroundColor:"#A967AA", 
+            position:"fixed",
+            left:"117px",
+            top:"177px"
+            }}><div style=
+            {{ 
+            height:"12px",
+            width:"14px",  
+            position:"fixed",
+            left:"122.17px",
+            top:"183.47px",
+            color:"#FFFFFF",
+            fontSize:"10.16px",
+            fontStyle:"Proxima Nova, Semibold",
+            textAlign:"left",
+            lineHeight:"12px"
+            }}>NK</div></div>
+            
+
+
+            <div style=
+            {{
+            borderRadius:"50%",  
+            height:"23px",
+            width:"23px",  
+            backgroundColor:"#F7931E", 
+            position:"fixed",
+            left:"106px",
+            top:"177px"
+            }}><div style=
+            {{ 
+            height:"12px",
+            width:"14px",  
+            position:"fixed",
+            left:"111.17px",
+            top:"183.47px",
+            color:"#FFFFFF",
+            fontSize:"10.16px",
+            fontStyle:"Proxima Nova, Semibold",
+            textAlign:"left",
+            lineHeight:"12px"
+            }}>NK</div></div>
             
             
 
+           <div style=
+            {{
+            borderRadius:"50%",  
+            height:"23px",
+            width:"23px",  
+            backgroundColor:"#CE242B", 
+            position:"fixed",
+            left:"96px",
+            top:"177px"
+            }}><div style=
+            {{ 
+            height:"12px",
+            width:"14px",  
+            position:"fixed",
+            left:"100.17px",
+            top:"183.47px",
+            color:"#FFFFFF",
+            fontSize:"10.16px",
+            fontStyle:"Proxima Nova, Semibold",
+            textAlign:"left",
+            lineHeight:"12px"
+            }}>NK</div></div>
 
 
+          <div style=
+            {{
+                width:"12px",
+                height:"20px",
+                textAlign:"left",
+                fontSize:"16px",
+                lineHeight:"20px",
+                color:"#FFFFFF",
+                position:"fixed",
+                left:"165.67px",
+                top:"184.33px"
+            }}>...</div>
 
-
-            <button style={{borderRadius:"50%",
-            borderColor:"#F5B7B1",
-            float:"left", 
-            height:"24px", 
-            width:"28px", 
-            fontSize:"10px", 
-            backgroundColor:"#F5B7B1", 
-            position:"absolute",
-            left:"68px",
-            marginTop:"8px",
-            color:"white",
-            borderStyle:"solid"}}>AL</button>
-            <button style={{borderRadius:"50%",
-            borderColor:"#EDBB99",
-            float:"left",
-            height:"24px",
-            width:"28px",
-            fontSize:"10px",
-            backgroundColor:"#EDBB99" 
-            ,position:"absolute",
-            left:"55px",
-            marginTop:"8px",
-            align:"left", 
-            color:"white", 
-            marginLeft:"0",
-            borderStyle:"solid"}}>LM</button>
-            <h3 style={{margin:"auto", position:"absolute",left:"105px"}}>
-            <pre>.. <button onClick={this.onevent} style={{backgroundColor:"#45CFE2",borderColor:"#45CFE2",
-            color:"white",borderStyle:"solid",fontSize:"15px"}}>Chemistry</button></pre></h3>
-
+            <div
+            onClick={this.onevent} 
+            style=
+            {{
+                width:"84px",
+                height:"19px",
+                textAlign:"left",
+                fontSize:"16px",
+                lineHeight:"18px",
+                color:"#FFFFFF",
+                position:"fixed",
+                left:"210px",
+                top:"181px"
+            }}>Chemistry</div>
             
-
-            <div style={{width:"5px", height:"5px", borderRadius:"50%", backgroundColor:"#7CFC00",
-            position:"absolute",left:"75px",marginTop:"26px"}}></div>
-            <div style={{width:"5px", height:"5px", borderRadius:"50%", backgroundColor:"#7CFC00",
-            position:"absolute",left:"88px",marginTop:"26px"}}></div>
-            <div style={{width:"5px", height:"5px", borderRadius:"50%", backgroundColor:"#7CFC00",
-            position:"absolute",left:"102px",marginTop:"26px"}}></div>
+            <div style=
+            {{
+                width:"6px",
+                height:"6px",
+                borderRadius:"50%",
+                backgroundColor:"#7AC943",
+                position:"fixed",
+                backgroundSize:"0.5px",
+                left:"109.5px",
+                top:"196.5px"
+            }}></div>
+            <div style=
+            {{
+                width:"6px",
+                height:"6px",
+                borderRadius:"50%",
+                backgroundColor:"#7AC943",
+                position:"fixed",
+                backgroundSize:"0.5px",
+                left:"121.5px",
+                top:"196.5px"
+            }}></div>
+            <div style=
+            {{
+                width:"6px",
+                height:"6px",
+                borderRadius:"50%",
+                backgroundColor:"#7AC943",
+                position:"fixed",
+                backgroundSize:"0.5px",
+                left:"133.5px",
+                top:"196.5px"
+            }}></div>
+            <div style=
+            {{
+                width:"6px",
+                height:"6px",
+                borderRadius:"50%",
+                backgroundColor:"#7AC943",
+                position:"fixed",
+                backgroundSize:"0.5px",
+                left:"144.5px",
+                top:"196.5px"
+            }}></div>
+            <div style=
+            {{
+                width:"6px",
+                height:"6px",
+                borderRadius:"50%",
+                backgroundColor:"#7AC943",
+                position:"fixed",
+                backgroundSize:"0.5px",
+                left:"155.5px",
+                top:"196.5px"
+            }}></div>
+            
             </div>   
             
-
-
-
-
-
-            <div style={{height:"334px",width:"240px",backgroundColor:"#F5F5F5"}}>
-            
-
-
-
-            <div style={{height:"305px",width:"240px"}}></div>
-             
+            <div style={{
+                height:"382px",
+                width:"302px",
+                position:"fixed",
+              
+                left:"90.5px",
+                top:"212px",
+                backgroundColor:"#F1F1F1"}}>            
        
-            <input style={{marginLeft:"10px"}}type="text" placeholder="Type your message"/>
-            <button style={{fontSize:"12px",
-            backgroundColor:"#F5F5F5",
-            borderStyle:"solid", 
-            borderColor:"#F5F5F5"}}><i className="fa fa-paperclip"></i></button>
-            <button style={{fontSize:"12px",backgroundColor:"#F5F5F5",borderStyle:"solid",
-            borderRadius:"50px"}}><i className="fa fa-paper-plane-o"></i></button>
+            <div style={{
+                width:"241px",
+                height:"36px",
+                position:"fixed",
+                left:"96px",
+                top:"545px",
+                backgroundColor:"#FFFFFF",
+                borderRadius:"19.5px 19.5px 19.5px 19.5px",
+                boxShadow: "0px 2px 2.5px #CBCBCB"
+                }}>
+
+            
+
+            <input style=
+            {{
+                width:"175px",
+                height:"15px",
+                position:"fixed",
+                left:"114px",
+                top:"554px",
+                color:"#CCCCCC",
+                fontSize:"12px",
+                lineHeight:"15px",
+                textAlign:"left",
+                fontStyle:"Montserrat, Light",
+                borderColor:"white",
+                borderStyle:"solid"
+            
+            }}
+            
+            type="text" 
+            placeholder="Type your message"/>
+
+
+            <div style=
+            {{
+                width:"11px",
+                height:"22px",
+                position:"fixed",
+                left:"302.65px",
+                top:"552.66px",
+                color:"#999999",
+                fontSize:"20px"
+
+                
+            }}><i className="fa fa-paperclip"></i></div>
             </div>
+            
+            <div style=
+            {{
+                width:"36px",
+                height:"36px",
+                position:"fixed",
+                left:"347px",
+                top:"546px",
+                backgroundColor:"#45CFE2",
+                borderRadius:"50%"
+                
+            }}><div style=
+            {{
+                width:"15.57px",
+                height:"14.14px",
+                position:"fixed",
+                left:"354px",
+                top:"555.43px",
+                color:"white",
+                fontSize:"20px"
 
+            }}>
+            <i className="fa fa-paper-plane-o"></i>
             </div>
-            
-            
-
-            
-
-
-            
-
-
-
-
-
+            </div>
+            </div>
+            </div>
             </div>
         );
 
